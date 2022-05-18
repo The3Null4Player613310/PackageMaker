06fbea7dabd5b5776a999ce1a33298fb56b803c2
N <nathanmunn21@gmail.com>
2022-05-18 02:55:11 -0300

    Added Licensing
    Specified GPLV2 licensing for all files.

--- PackageMaker/__init__.py
+++ PackageMaker/__init__.py
@@ -1,4 +1,3 @@
-<!---
 # PackageMaker: Make and publish packages using an intuitive interface.
 # Copyright (C) 2022  Nathan A. Munn
 # 
@@ -14,7 +13,3 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
---->
-
-# PackageMaker
-Make and publish packages using an intuitive interface.
